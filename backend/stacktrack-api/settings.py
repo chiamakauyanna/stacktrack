893e25db57973436fa35d4223a802cdec73b96f5
Chiamaka Uyanna <chiamakauyanna@gmail.com>
2025-11-13 16:21:53 +0100

    Restructure project: rename Django project folder

--- backend/stacktrack-api/settings.py
+++ backend/stacktrack-api/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'stacktrack.urls'
+ROOT_URLCONF = 'stacktrack-api.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'stacktrack.wsgi.application'
+WSGI_APPLICATION = 'stacktrack-api.wsgi.application'
 
 # DATABASE
 DATABASES = {
